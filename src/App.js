be1c8c23dee898737089d882bba2c4153fdf8ebe
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-03-23 16:15:47 +0100

    Implement fetching jsonplaceholders as robots

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 const App = () => {
+  const [robots, setRobots] = useState([]);
   const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        return response.json();
+      })
+      .then(robots => {
+        setRobots(robots);
+      });
+  });
 
   const onSearchChange = event => {
     setSearch(event.target.value);
